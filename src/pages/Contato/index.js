03892ef31580b24551980c6db386c4e1c3c297f3
pkaislan123 <comprasaislan@gmail.com>
2022-02-28 19:00:31 -0300

    Change

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -73,49 +73,15 @@ const Contato = () => {
                                 amplo pátio aos utilizadores, contando com restaurante e vestiário.
                                 Estamos a 103 km de Coromandel, a 78 km de Paracatu, atendemos
                                 Produtores Rurais de toda a região do Noroeste de Minas.
-                                <br></br>  <br></br>
-                                Desde 2018 a administração da LD Armazém vem investindo na modernização do armazém,
-                                nossas instalações estão prontas a receber você cliente
-                                e você amigo caminhoneiro.
+                              
                             </Typography>
                         </Grid>
-                        <Grid item xs={12} sm={12} md={12} lg={5} xl={5} style={{ paddingLeft: 50 }}>
-                            <img alt="Armazém"
-                                src={imgExemplo}
-                            />
-                        </Grid>
+                       
                         <Grid item xs={12} sm={12} md={12} lg={1} xl={1} >
                         </Grid>
                     </Grid>
 
-                    <div style={{ backgroundColor: '#E6E6FA', color: 'black' }}>
-                        <Grid
-                            container
-                            direction="column"
-                            justifyContent="center"
-                            alignItems="center"
-                             item xs={12} sm={12} md={12} lg={12} xl={12}
-                            style={{ paddingBottom: 100 }}
-                        >
-                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} >
-                                <Typography component="h1" variant="h3" style={{ textAlign: 'center', paddingTop: 150, paddingBottom: 100 }}>
-                                    Onde Estamos?
-                                </Typography>
-                            </Grid>
-                            <iframe
-                                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDCL-kmB8NJF2jWAFU8emWx7ldg-B16-nc&q=LD+ARMAZ%C3%89M+GERAIS+LTDA,Guarda-Mor+MG&maptype=satellite"
-                                width={width}
-                                height={height}
-                                title="Localização no Mapa"
-                                style={{ paddingBottom: 30 }}
-                                allowFullScreen={true}
-                                onLoad={() => checkDimenssoes()}
-
-                            >
-                            </iframe>
-
-                        </Grid>
-                    </div>
+                   
                 </div>
             </div>
 
